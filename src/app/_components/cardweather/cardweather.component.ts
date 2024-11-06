663928282f634a18c8448f0321742cda27872dc1
unknown <jeanfire.fg@gmail.com>
2024-11-06 16:52:32 -0300

    feat: create component cardweather

---
+++ src/app/_components/cardweather/cardweather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardweather',
+  standalone: true,
+  imports: [],
+  templateUrl: './cardweather.component.html',
+  styleUrl: './cardweather.component.css'
+})
+export class CardweatherComponent {
+
+}
