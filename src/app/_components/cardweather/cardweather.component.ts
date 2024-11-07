47b90f4483eabcf48c380e33ae9c20d16c14d788
unknown <jeanfire.fg@gmail.com>
2024-11-07 10:43:16 -0300

    feat: icons dinamics colors completed

--- src/app/_components/cardweather/cardweather.component.ts
+++ src/app/_components/cardweather/cardweather.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'cardweather',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cardweather.component.html',
 })
 export class CardweatherComponent {
@@ -15,6 +15,7 @@ export class CardweatherComponent {
   @Input() description!: string;
   @Input() temp!: string;
   icontype: string = '';
+  iconColor: string = '';
 
   ngOnInit() {}
 
@@ -22,6 +23,7 @@ export class CardweatherComponent {
     // Sempre que qualquer valor de @Input mudar, chamar a função para atualizar o ícone
     if (changes['temp']) {
       this.iconMoment();
+      this.updateIconColor();
       console.log(this.icontype);
     }
   }
@@ -32,19 +34,38 @@ export class CardweatherComponent {
         break;
 
       case 'Clouds':
-        this.icontype = 'filter_dramas'; // Ícone para "Clouds"
+        this.icontype = 'cloud'; // Ícone para "Clouds"
         break;
 
       case 'Rain':
-        this.icontype = 'brightness_7'; // Ícone para "Rain"
+        this.icontype = 'rainy_snow'; // Ícone para "Rain"
         break;
 
       case 'Thunderstorm':
-        this.icontype = 'flash_on'; // Ícone para "Thunderstorm"
+        this.icontype = 'thunderstorm'; // Ícone para "Thunderstorm"
         break;
 
       default:
         this.icontype = 'looks'; // Ícone para clima desconhecido
+        break;
+    }
+  }
+  updateIconColor() {
+    switch (this.temp) {
+      case 'Clear':
+        this.iconColor = 'text-yellow-500'; // Sol
+        break;
+      case 'Clouds':
+        this.iconColor = 'text-gray-500'; // Nublado
+        break;
+      case 'Rain':
+        this.iconColor = 'text-slate-400'; // Chuva
+        break;
+      case 'Thunderstorm':
+        this.iconColor = 'text-slate-500'; // Tempestade
+        break;
+      default:
+        this.iconColor = 'text-slate-500'; // Clima desconhecido
         break;
     }
   }
