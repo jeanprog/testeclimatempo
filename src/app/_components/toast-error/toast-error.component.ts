fad61e90d0dc7285b0a41e680730a1fef84675c0
Suporte 4 <suporte4@laser.local>
2024-11-07 17:40:47 -0300

    bug: suggestion

--- src/app/_components/toast-error/toast-error.component.ts
+++ src/app/_components/toast-error/toast-error.component.ts
@@ -7,15 +7,7 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
   templateUrl: './toast-error.component.html',
   styleUrl: './toast-error.component.css',
 })
-export class ToastErrorComponent implements OnChanges {
+export class ToastErrorComponent {
   @Input() show: boolean = false;
   @Input() message: string = '';
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['show'] && this.show) {
-      this.show === true;
-      console.log(this.show);
-      setTimeout(() => (this.show = false), 10000); // Esconde após 10s
-    }
-  }
 }
